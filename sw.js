ea6884c8fe1ebc8c470f6b3920d3ba2884c9be1a
Gabriel Haddad <158028048+gabrielhcg05@users.noreply.github.com>
2026-05-25 17:57:05 -0300

    Atualiza MODOS GREGOS

--- sw.js
+++ sw.js
@@ -16,7 +16,5 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
