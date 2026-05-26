12b19e61c7a1deaa49a150ba5c2ec751ac12c2d3
Gabriel Haddad <158028048+gabrielhcg05@users.noreply.github.com>
2026-05-26 19:36:47 -0300

    Atualiza modo de atualização

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'scales-v1';
-const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+const ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -16,17 +16,12 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(fetch(e.request));
+    return;
+  }
+
   e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const copy = res.clone();
-
-        caches.open(CACHE).then(cache => {
-          cache.put(e.request, copy);
-        });
-
-        return res;
-      })
-      .catch(() => caches.match(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
